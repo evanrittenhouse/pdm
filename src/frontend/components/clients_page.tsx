4971172af259688aafc2df515f62a52d9fe9b9a9
Evan Rittenhouse <evan.rittenhouse@workboard.com>
2021-11-20 22:38:08 -0600

    Add images to folder and render on the clients page. More CSS formatting needed

--- src/frontend/components/clients_page.tsx
+++ src/frontend/components/clients_page.tsx
@@ -1,13 +1,35 @@
 import { Link } from 'react-router-dom';
+import { fourD, apple, cisco, moma, nasa, quantum, sprint, stanford, sybase } from './images/imageIndex.js';
 
 export default function ClientsPage() {
   return (
-    <div className="container">
-      <Link to="/team">
-        <p>Vodafone</p>
+    <div className="image-container">
+      <ImageBox imagePath={fourD} clientName="4D Systems" clientPath="https://4dsystems.com.au" />
+      <ImageBox imagePath={apple} clientName="Apple, Inc." clientPath="https://www.apple.com" />
+      <ImageBox imagePath={cisco} clientName="Cisco Systems, Inc." clientPath="https://www.apple.com" />
+      <ImageBox imagePath={moma} clientName="San Francisco Museum of Modern Art" clientPath="https://www.apple.com" />
+      <ImageBox imagePath={nasa} clientName="NASA Ames Research Lab" clientPath="https://www.apple.com" />
+      <ImageBox imagePath={quantum} clientName="Quantum" clientPath="https://www.apple.com" />
+      <ImageBox imagePath={sprint} clientName="Sprint" clientPath="https://www.apple.com" />
+      <ImageBox imagePath={stanford} clientName="Stanford University" clientPath="https://www.apple.com" />
+      <ImageBox imagePath={sybase} clientName="Sybase, Inc." clientPath="https://www.apple.com" />
+    </div>
+  );
+}
+
+interface ImageBoxProps {
+  imagePath: string;
+  clientPath: string;
+  clientName: string;
+}
+
+function ImageBox({ imagePath, clientPath, clientName }: ImageBoxProps) {
+  return (
+    <div>
+      <Link to={clientPath}>
+        <img className="image" src={imagePath} alt={clientName} />
+        <p>{clientName}</p>
       </Link>
-      <p>Agritech</p>
-      <p>SJJS</p>
     </div>
   );
 }
---
+++ src/frontend/components/images/imageIndex.js
@@ -0,0 +1,11 @@
+import fourD from './4d.jpeg';
+import apple from './apple.svg';
+import cisco from './cisco.png';
+import moma from './moma.png';
+import nasa from './nasa.png';
+import quantum from './quantum.png';
+import sprint from './sprint.png';
+import stanford from './stanford.png';
+import sybase from './sybase.png';
+
+export { fourD, apple, cisco, moma, nasa, quantum, sprint, stanford, sybase };
