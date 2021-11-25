15ccc5149766bebea5b4806e5ae568f49af0c3ac
Evan Rittenhouse <evan.rittenhouse@workboard.com>
2021-11-24 21:10:55 -0600

    Adding state changes to navbar

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -3,19 +3,27 @@ import ClientsPage from './pages/clients_page';
 import TeamPage from './pages/team_page';
 import HomePage from './pages/home_page';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
 import './App.css';
 import './css/main.css';
 
 function App() {
+  // TODO: implement paddingTop to be the height of the navBar - currently a placeholder of 20px
+  // useEffect(
+
+  //     );
+
   return (
     <div className="App">
       <Router>
         <NavBar />
-        <div>
+        <div style={{ paddingTop: '20px' }}>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/clients" element={<ClientsPage />}></Route>
             <Route path="/team" element={<TeamPage />} />
+            <Route path="/services" element={<ClientsPage />}></Route>
+            <Route path="/careers" element={<TeamPage />}></Route>
           </Routes>
         </div>
       </Router>
--- src/frontend/components/nav_bar.tsx
+++ src/frontend/components/nav_bar.tsx
@@ -1,79 +1,33 @@
-// import { useState, useEffect } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, MouseEvent } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import '../css/main.css';
 
 export default function NavBar() {
+  // TODO: get navbar button to show if the user is on mobile
+  const [state, setState] = useState<boolean>(false);
+
+  const handleMouseEvent = (e: MouseEvent<HTMLButtonElement>) => setState(!state);
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
       <Container>
         <Navbar.Brand href="/">PDM</Navbar.Brand>
-        <Nav.Link className="nav-link-element" href="/clients">
+        <Nav.Link className="nav-link-element" href="/clients" onMouseEnter={handleMouseEvent}>
           Clients
         </Nav.Link>
         <Nav.Link className="nav-link-element" href="/team">
-          Team
+          {state ? 'Team' : 'Placeholder'}
         </Nav.Link>
-        <Nav.Link className="ml-auto p-2 nav-link-element" href="/contact">
+        <Nav.Link className="nav-link-element" href="/services">
+          Services
+        </Nav.Link>
+        <Nav.Link className="nav-link-element" href="/careers">
+          Careers
+        </Nav.Link>
+        <Nav.Link className="ml-auto p-1 nav-link-element" href="/contact">
           Contact Us
         </Nav.Link>
       </Container>
     </Navbar>
   );
 }
-
-// export function NavBar({ showingHeader = true }: NavBarType) {
-//   // TODO: make contact button work, routes won't render for some reason
-//   // float contact-button right
-//   const [headerTextColor, setHeaderTextColor] = useState('white');
-
-//   useEffect(() => {
-//     const headerTextColorTemp = showingHeader ? 'white' : 'black';
-//     setHeaderTextColor(headerTextColorTemp);
-//   }, [showingHeader]);
-
-//   return (
-//     <div id="App-header">
-//       <ul id="nav" style={{ color: headerTextColor }} className="container-fluid">
-//         <li>
-//           <Link to="/" id="button-a-copy">
-//             PDM
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to="/clients" style={{ color: headerTextColor }}>
-//             Clients
-//           </Link>
-//         </li>
-//         <li>
-//           <NavButton navLocation={'/about'} buttonText="Contact Us" />
-//         </li>
-//         <li>
-//           <Link to="/team" style={{ color: headerTextColor }}>
-//             Team
-//           </Link>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// }
-
-// interface NavButtonType {
-//   navLocation: string;
-//   buttonText: string;
-//   buttonColor?: string;
-// }
-
-// function NavButton({ navLocation, buttonText, buttonColor = 'white' }: NavButtonType) {
-//   const navigate = useNavigate();
-
-//   function handleClick(): void {
-//     navigate(navLocation);
-//   }
-
-//   return (
-//     <button onClick={handleClick} className="navButton" style={{ color: buttonColor }}>
-//       {buttonText}
-//     </button>
-//   );
-// }
