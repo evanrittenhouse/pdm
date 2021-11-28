a29c408059a5169772e148cc91c4600f76a41c65
Evan Rittenhouse <evanrittenhouse@gmail.com>
2021-11-28 13:08:07 -0600

    Implement modal closing with Esc, click outside, and click on all buttons

--- src/frontend/components/contact_modal.tsx
+++ src/frontend/components/contact_modal.tsx
@@ -1,28 +1,34 @@
-import React, { useState, useRef, MouseEvent, useCallback } from 'react';
+import React, { useState, useRef, MouseEvent, useCallback, useEffect } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
 export interface ContactModalProps {
   shown: boolean;
   stateFunc: Function;
-  ref: any;
+  modalRef: any;
+  closeButtonRef: any;
 }
 
-const ContactUsModal = ({ shown, stateFunc, ref }: ContactModalProps) => {
+const ContactUsModal = ({ shown, stateFunc, modalRef, closeButtonRef }: ContactModalProps) => {
   const close = () => stateFunc(false);
-  const onClickOutside = (event: KeyboardEvent) => {
-    if (ref.contains(event.target)) return;
-    close();
-  };
-  const onKeyDown = (event: KeyboardEvent) => {
-    if (event.keyCode === 27) stateFunc(false);
+
+  const onKeyDown = (event: KeyboardEvent): void => {
+    if (event.key === 'Escape') stateFunc(false);
   };
 
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown, false);
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown, false);
+    };
+  });
+
   return (
-    <Modal show={shown} ref={ref}>
+    <Modal show={shown} ref={modalRef}>
       <Modal.Header closeButton>
         <Modal.Title>Contact Us</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body onClick={(e: MouseEvent) => e.stopPropagation()}>
         Tell us how we can build the perfect database structure for your business.
         <ContactUsForm />
       </Modal.Body>
--- src/frontend/components/contact_us_button.tsx
+++ src/frontend/components/contact_us_button.tsx
@@ -7,24 +7,16 @@ import '../css/main.css';
 export default function ContactUsButton() {
   const [showModal, showModalState] = useState<boolean>(false);
   const modalRef = useRef(null);
-
-  const showModalFunc = (): void => {
-    showModalState(true);
-    toggleScrollLock();
-  };
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
 
   const handleClick = (e: MouseEvent<HTMLButtonElement>): void => {
-    console.log(e.currentTarget.type);
     showModalState(!showModal);
+    toggleScrollLock();
 
-    if (showModal === true) {
-      document.querySelector('html').classList.toggle('scroll-lock');
-    }
+    // if (showModal === true) {
+    //   document.querySelector('html').classList.toggle('scroll-lock');
+    // }
   };
-
-  // const onClickOutside = (event: MouseEvent): void => {
-  //   if (showModal && modalRef!.current!.contains(event.target)) return;
-  // };
 
   const toggleScrollLock = (): void => {
     document.querySelector('html').classList.toggle('scroll-lock');
@@ -33,7 +25,7 @@ export default function ContactUsButton() {
   return (
     <Button className='ml-auto p-2' id='contact-us-button' onClick={handleClick}>
       Contact Us
-      <ContactUsModal shown={showModal} stateFunc={handleClick} ref={modalRef} />
+      <ContactUsModal shown={showModal} stateFunc={handleClick} modalRef={modalRef} closeButtonRef={closeButtonRef} />
     </Button>
   );
 }
